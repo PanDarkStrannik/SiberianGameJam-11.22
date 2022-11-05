63b0855fdb2cf50e5b2eab55c3f1c258fb596906
DarkStrannik <49794843+PanDarkStrannik@users.noreply.github.com>
2022-11-05 15:06:14 +0300

    SIB-22 Sound controller

--- Assets/Scripts/GameClient/GameManager/Controllers/MusicController.cs
+++ Assets/Scripts/GameClient/GameManager/Controllers/MusicController.cs
@@ -1,9 +1,60 @@
+using System.Collections;
+using System.Linq;
 using GameCore;
+using UnityEngine;
 
 namespace GameClient
 {
     public class MusicController : BaseGameManagerModuleController<MusicModule>
     {
+        private GameManager _gameManager;
+        private LevelSwitchController _levels;
+        private AudioSource _effectsSound;
+        private AudioSource _locationSound;
 
+        private Coroutine _locationSoundCoroutine;
+        private Coroutine _effectsSoundCoroutine;
+
+        protected override void InternalInitialize()
+        {
+            _gameManager = GameManager.Instance;
+            _gameManager.SubscribeOnInitialize(OnInitialize);
+            _locationSound = _gameManager.gameObject.AddComponent<AudioSource>();
+            _effectsSound = _gameManager.gameObject.AddComponent<AudioSource>();
+        }
+
+        private void OnInitialize()
+        {
+            _levels.OnGameLevelLoaded += PlaySoundByLocation;
+        }
+
+        private void PlaySoundByLocation(LevelSwitchModule.GameLevels level)
+        {
+            var musicData = Data.GetLevelSound(level);
+            PlayLocationSound(musicData.PlayOnShot, musicData.LoopedAudio);
+        }
+
+        private void PlayLocationSound(params AudioClip[] audios)
+        {
+            if (_locationSoundCoroutine != null)
+            {
+                _gameManager.StopCoroutine(_locationSoundCoroutine);
+            }
+            _locationSoundCoroutine = _gameManager.StartCoroutine(PlaySeq(_locationSound, audios));
+        }
+
+        private static IEnumerator PlaySeq(AudioSource source, params AudioClip[] audios)
+        {
+            for (var i=0; i < audios.Length - 1; i++)
+            {
+                var audio = audios[i];
+                source.PlayOneShot(audio);
+                yield return new WaitForSeconds(audio.length);
+            }
+
+            source.clip = audios.Last();
+            source.loop = true;
+            source.Play();
+        }
     }
 }
--- Assets/Scripts/GameClient/GameManager/Modules/MusicModule.cs
+++ Assets/Scripts/GameClient/GameManager/Modules/MusicModule.cs
@@ -1,12 +1,28 @@
 using System;
+using System.Collections.Generic;
 using GameCore;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace GameClient
 {
-    [Serializable]
     public class MusicModule : BaseGameManagerModule
     {
-        [SerializeField] private int test;
+        [SerializeField] private Dictionary<LevelSwitchModule.GameLevels, LevelMusicData> _levelSoundPair = new Dictionary<LevelSwitchModule.GameLevels, LevelMusicData>();
+
+        public LevelMusicData GetLevelSound(LevelSwitchModule.GameLevels level)
+        {
+            return _levelSoundPair[level];
+        }
+
+        [Serializable, HideReferenceObjectPicker]
+        public class LevelMusicData
+        {
+            [SerializeField] private AudioClip _playOnShot;
+            [SerializeField] private AudioClip _loopedAudio;
+
+            public AudioClip PlayOnShot => _playOnShot;
+            public AudioClip LoopedAudio => _loopedAudio;
+        }
     }
 }
